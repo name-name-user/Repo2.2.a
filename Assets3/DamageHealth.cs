779951dfb05f1ee1309fe02c245552828600f102
name-name-user <luke.is.southard@gmail.com>
2019-03-29 16:59:54 -0400

    s

---
+++ Assets3/DamageHealth.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHealth : MonoBehaviour
+{
+    void OnTriggerEnter2d(Collider2d col)
+    {
+        HealthBarScript.health -= 10f;
+    }
+}
---
+++ Assets3/MoveScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    float dirX, dirY;
+    public float moveSpeed = 5f;
+    Rigidbody2d rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2d> ();
+    }
+
+    
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal");
+        dirY = Input.GetAxis("Vertical");
+    }
+
+    void fixedUpdate()
+    {
+        rb.Velocity = new Vector2(dirX * moveSpeed);
+    }
+}
